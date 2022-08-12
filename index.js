8a1628b99a051cf4a64344b6ef568618cc9427f0
Kenneth Latour <kenneth.latourii@gmail.com>
2022-08-12 10:31:44 -0500

    commit tree cd636079c964407a264cab5f5c2b3fda490d8a96

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+//   function superbowlWin (result) {
+//     record.result === 'W';
+//     return
+//     }
+// record.find(superbowlWin).year
+function superbowlWin(array){
+    const newObj = array.find(array => array.result === 'W')
+    return newObj ? newObj.year : undefined
+}
